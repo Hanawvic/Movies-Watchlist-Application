f0f95076eef2cccb604a8a5a41c87fe4901b288e
Heni Bouafia <bouafia.heni@gmail.com>
2023-04-28 00:04:39 +0100

    reformating config

--- movie_library/config.py
+++ movie_library/config.py
@@ -9,9 +9,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     MONGODB_URI = os.environ.get('MONGODB_URI')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
     RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY")
     RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
 
