70d408f67dc17c063cfba42408af8bae436a9ed1
Heni Bouafia <bouafia.heni@gmail.com>
2023-04-28 01:34:40 +0100

    updating packages

--- movie_library/__init__.py
+++ movie_library/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from pymongo import MongoClient
-from flask_bootstrap import Bootstrap5
 from flask_mail import Mail
 from movie_library.config import Config
 
 
-bootstrap = Bootstrap5()
 mail = Mail()
 client = MongoClient(Config.MONGODB_URI)
 db = client.movies
@@ -14,7 +12,6 @@ db = client.movies
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    bootstrap.init_app(app)
     mail.init_app(app)
     # app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
     with app.app_context():
